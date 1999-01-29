8ae38a54649620fda13bcf79eb9fe4d3dffd4909
Márcio Pessoa <marcio.pessoa@sciemon.com>
1999-01-29 00:15:31 -0200

    Just for reference

---
+++ CODE/Drica.h
@@ -0,0 +1,5 @@
+# include <graphics.h>
+
+/** Defini‡ao: Definicoes de constantes do programa. ***********************/
+#define TITLE "DRICA-Controler ver.: 0.1b" /** Titulo. *********************/
+#define COMENTS "The Wizards InUtilities" /** Comentarios. *****************/
